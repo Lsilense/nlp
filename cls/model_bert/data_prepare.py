3ea594fbbeecb7bb6d335b70566853f36cc4ad74
Lsilense <838440083@qq.com>
2025-10-15 21:53:46 +0800

    修改

--- cls/model_bert/data_prepare.py
+++ cls/model_bert/data_prepare.py
@@ -9,13 +9,17 @@ dataset = load_dataset("imdb")
 # 加载预训练的 BERT Tokenizer
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+
 # 数据预处理函数：将文本转化为 BERT 可接受的输入格式
 def preprocess_function(examples):
-    return tokenizer(examples['text'], padding="max_length", truncation=True, max_length=512)
+    return tokenizer(
+        examples["text"], padding="max_length", truncation=True, max_length=512
+    )
+
 
 # 对数据集进行预处理
-train_dataset = dataset['train'].map(preprocess_function, batched=True)
-test_dataset = dataset['test'].map(preprocess_function, batched=True)
+train_dataset = dataset["train"].map(preprocess_function, batched=True)
+test_dataset = dataset["test"].map(preprocess_function, batched=True)
 
 # 转换为 PyTorch DataLoader
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
